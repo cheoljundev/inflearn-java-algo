c3c1980ea380fe2b0a227e502aed15b9d7663e5a
cheoljundev <cheoljundev@gmail.com>
2025-02-10 06:10:33 +0900

    1. 문자 찾기

---
+++ src/string/FindChar.java
@@ -0,0 +1,17 @@
+package string;
+
+import java.util.Scanner;
+
+public class FindChar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next().toUpperCase();
+        char t = Character.toUpperCase(sc.next().charAt(0));
+
+        int answer = 0;
+
+        answer = (int) str.chars().filter(ch -> ch == t).count();
+
+        System.out.println(answer);
+    }
+}
