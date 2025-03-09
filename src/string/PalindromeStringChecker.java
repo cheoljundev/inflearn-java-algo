040d3b60ae477fc9ab1876dcd3b7497ffd44ef21
cheoljundev <cheoljundev@gmail.com>
2025-03-09 18:38:17 +0900

    팰린드롬(replaceAll 정규식이용)

---
+++ src/string/PalindromeStringChecker.java
@@ -0,0 +1,30 @@
+package string;
+
+import java.util.Scanner;
+
+public class PalindromeStringChecker {
+
+    public static String solution(String str) {
+
+        str = str.toLowerCase().replaceAll("[^a-z]", "");
+        int len = str.length();
+        for (int i = 0; i < len / 2; i++) {
+            if (str.charAt(i) != str.charAt(len - i - 1)) {
+                return "NO";
+            }
+        }
+
+        return "YES";
+
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        System.out.println(solution(str));
+
+    }
+}
