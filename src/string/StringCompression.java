90d2e034c36c17c358c4b6d426a89f6e5c15fea1
cheoljundev <cheoljundev@gmail.com>
2025-03-21 02:05:49 +0900

    문자열 압축 : 내 풀이

---
+++ src/string/StringCompression.java
@@ -0,0 +1,48 @@
+package string;
+
+import java.util.Scanner;
+
+public class StringCompression {
+
+    private static String getSolution(String str) {
+        StringBuilder answer = new StringBuilder();
+
+        char[] chars = str.toCharArray();
+
+        char before = '\0';
+        int count = 0;
+        for (int i = 0; i < chars.length; i++) {
+            if (i == 0) {
+                before = chars[i];
+                count = 1;
+                answer.append(before);
+            } else {
+                if (before == chars[i]) {
+                    count++;
+                } else {
+                    if (count != 1) {
+                        answer.append(count);
+                    }
+                    before = chars[i];
+                    count = 1;
+                    answer.append(before);
+                }
+            }
+        }
+
+        if (count != 1) {
+            answer.append(count);
+        }
+
+
+        return answer.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        System.out.println(getSolution(str));
+
+    }
+}
