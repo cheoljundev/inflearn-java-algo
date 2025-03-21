13b95d4a737696ec812bbda85dc6d3fb295d8b9a
cheoljundev <cheoljundev@gmail.com>
2025-03-21 16:31:32 +0900

    문자열 압축 : 강의 문제풀이

--- src/string/StringCompression.java
+++ src/string/StringCompression.java
@@ -38,11 +38,32 @@ public class StringCompression {
         return answer.toString();
     }
 
+    private static String solution2(String str) {
+        String answer = "";
+        str += " ";
+
+        int cnt = 1;
+
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.charAt(i) == str.charAt(i + 1)) {
+                cnt++;
+            } else {
+                answer += str.charAt(i);
+                if (cnt > 1) {
+                    answer += String.valueOf(cnt);
+                }
+                cnt = 1;
+            }
+        }
+
+        return answer;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
 
-        System.out.println(solution(str));
+        System.out.println(solution2(str));
 
     }
 }
