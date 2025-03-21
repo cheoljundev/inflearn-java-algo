601a9bd2bbccd971a8d5117cc6ec0c56fbc803e5
cheoljundev <cheoljundev@gmail.com>
2025-03-21 23:02:32 +0900

    암호 : 강의 풀이

--- src/string/Password.java
+++ src/string/Password.java
@@ -27,12 +27,25 @@ public class Password {
 
     }
 
+    private static String solution2(int n, String str) {
+        String answer = "";
+
+        for (int i = 0; i < n; i++) {
+            String tmp = str.substring(0, 7).replace('#', '1').replace('*', '0');
+            int num = Integer.parseInt(tmp, 2);
+            answer += (char) num;
+            str = str.substring(7);
+        }
+
+        return answer;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int cnt = sc.nextInt();
         sc.nextLine();
         String str = sc.nextLine();
 
-        System.out.println(solution(cnt, str));
+        System.out.println(solution2(cnt, str));
     }
 }
