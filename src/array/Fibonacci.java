27c83e89a11b0345d2aaaf4585c3a83b10475091
cheoljundev <cheoljundev@gmail.com>
2025-03-23 01:15:57 +0900

    피보나치 수열 : 내 풀이

---
+++ src/array/Fibonacci.java
@@ -0,0 +1,29 @@
+package array;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+
+    private static int[] solution(int n) {
+        int[] answer = new int[n];
+
+        answer[0] = 1;
+        answer[1] = 1;
+
+        for (int i = 2; i < n; i++) {
+            answer[i] = answer[i - 1] + answer[i - 2];
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int x : solution(n)) {
+            System.out.print(x + " ");
+        }
+
+    }
+}
