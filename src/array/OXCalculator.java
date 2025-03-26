bd871b25b3b6b6ed631188db74b14de46edfd63b
cheoljundev <cheoljundev@gmail.com>
2025-03-26 23:32:42 +0900

    점수계산 : 강의 풀이

--- src/array/OXCalculator.java
+++ src/array/OXCalculator.java
@@ -27,6 +27,21 @@ public class OXCalculator {
 
     }
 
+    private static int solution2(int n, int[] arr) {
+        int answer = 0;
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 1) {
+                cnt++;
+                answer += cnt;
+            } else {
+                cnt = 0;
+            }
+        }
+
+        return answer;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -37,7 +52,7 @@ public class OXCalculator {
             arr[i] = sc.nextInt();
         }
 
-        System.out.println(solution(n, arr));
+        System.out.println(solution2(n, arr));
 
     }
 }
