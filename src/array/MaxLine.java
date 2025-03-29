c7a6231e6a110ccfcab0e504ab7ba687a85b1e6c
cheoljundev <cheoljundev@gmail.com>
2025-03-29 21:49:42 +0900

    격자판 최대합 : 강의 풀이

--- src/array/MaxLine.java
+++ src/array/MaxLine.java
@@ -56,18 +56,55 @@ public class MaxLine {
         return answer;
     }
 
+    private static int solution2(int n, int[][] arr) {
+        int answer = Integer.MIN_VALUE;
+
+        int sum1, sum2;
+
+        for (int i = 0; i < n; i++) {
+            sum1 = sum2 = 0;
+
+            for (int j = 0; j < n; j++) {
+                sum1 += arr[i][j];
+                sum2 += arr[j][i];
+            }
+            answer = Math.max(answer, sum1);
+            answer = Math.max(answer, sum2);
+        }
+
+        sum1 = sum2 = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum1 += arr[i][i];
+            sum2 += arr[i][n-i-1];
+        }
+        answer = Math.max(answer, sum1);
+        answer = Math.max(answer, sum2);
+
+        return answer;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] arr = new int[n*n];
+        //int[] arr = new int[n*n];
+        int[][] arr = new int[n][n];
 
-        sc.nextLine();
+        //sc.nextLine();
 
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        /*
         for (int i = 0; i < n*n; i++) {
             arr[i] = sc.nextInt();
         }
+        */
 
-        System.out.println(solution(n, arr));
+        System.out.println(solution2(n, arr));
 
     }
 }
