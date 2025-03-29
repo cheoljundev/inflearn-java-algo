1441825da60dfb74e5bdf0d067c9f58d93aa7ab5
cheoljundev <cheoljundev@gmail.com>
2025-03-30 00:09:24 +0900

    봉우리 : 강의 풀이

--- src/array/Mountain.java
+++ src/array/Mountain.java
@@ -27,6 +27,33 @@ public class Mountain {
         return answer;
     }
 
+    private static int solution2(int n, int[][] arr) {
+        int answer = 0;
+
+        int[] dx = {-1, 0, 1, 0};
+        int[] dy = {0, 1, 0, -1};
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                boolean flag = true;
+                for (int k = 0; k < 4; k++) {
+                    int nx = i+dx[k];
+                    int ny = j+dy[k];
+                    if (nx>=0 && ny >= 0 && nx < n && ny < n && arr[nx][ny] >= arr[i][j]) {
+                        flag = false;
+                        break;
+                    }
+                }
+
+                if (flag) {
+                    answer++;
+                }
+            }
+        }
+
+        return answer;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -40,7 +67,7 @@ public class Mountain {
         }
 
 
-        System.out.println(solution(n, arr));
+        System.out.println(solution2(n, arr));
 
     }
 }
