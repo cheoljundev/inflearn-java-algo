1f3fa7322f7308cd483284b24f720008d4c40980
cheoljundev <cheoljundev@gmail.com>
2025-04-01 01:51:33 +0900

    멘토링 : 강의 풀이 주석 달기

--- src/array/Mentoring.java
+++ src/array/Mentoring.java
@@ -7,28 +7,20 @@ public class Mentoring {
     private static int solution(int n, int m, int[][] arr) {
         int answer = 0;
 
-        for (int i = 1; i<= n; i++) {
-            for (int j = 1; j <=n; j++) {
-                int cnt = 0;
-                for (int k = 0; k < m; k++) {
-                    int pi = 0, pj = 0;
+        for (int i = 1; i <= n; i++) {        // 멘토 후보 i
+            for (int j = 1; j <= n; j++) {    // 멘티 후보 j
+                int cnt = 0;                  // i가 j보다 항상 등수 높은 횟수
+                for (int k = 0; k < m; k++) { // 각 테스트에 대해
+                    int pi = 0, pj = 0;       // i와 j의 등수 (0-based index)
                     for (int s = 0; s < n; s++) {
-                        if (arr[k][s] == i) {
-                            pi = s;
-                        }
-                        if (arr[k][s] == j) {
-                            pj = s;
-                        }
+                        // 시험 k에서 등수 s에 해당하는 학생이 i면 pi에 등수 저장
+                        if (arr[k][s] == i) pi = s;
+                        // 시험 k에서 등수 s에 해당하는 학생이 j면 pj에 등수 저장
+                        if (arr[k][s] == j) pj = s;
                     }
-
-                    if (pi < pj) {
-                        cnt++;
-                    }
-
-                    if (cnt == m) {
-                        answer++;
-                    }
+                    if (pi < pj) cnt++; // i가 j보다 등수가 높으면 (숫자가 작으면)
                 }
+                if (cnt == m) answer++; // 모든 테스트에서 i가 j보다 등수가 높으면
             }
         }
 
