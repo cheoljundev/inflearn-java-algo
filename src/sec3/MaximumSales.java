eec7d5d3136a79c6e62e51f0833bfc2bdd736dce
cheoljundev <cheoljundev@gmail.com>
2025-04-02 02:48:22 +0900

    최대 매출 : 강의 풀이 (sliding window)

--- src/sec3/MaximumSales.java
+++ src/sec3/MaximumSales.java
@@ -4,16 +4,16 @@ import java.util.Scanner;
 
 public class MaximumSales {
     //작동하지만, Time Limit.
-    private static int solution(int n, int m, int[] arr) {
+    private static int solution(int n, int k, int[] arr) {
         int answer = 0;
-        int[] pos = new int[m];
-        for (int i = 0; i < m; i++) {
+        int[] pos = new int[k];
+        for (int i = 0; i < k; i++) {
             pos[i] = i;
         }
 
-        while (pos[m-1] < n) {
+        while (pos[k-1] < n) {
             int sum = 0;
-            for (int i = 0; i < m; i++) {
+            for (int i = 0; i < k; i++) {
                 sum += arr[pos[i]++];
             }
             if (sum > answer) {
@@ -24,16 +24,31 @@ public class MaximumSales {
         return answer;
     }
 
+    private static int solution2(int n, int k, int[] arr) {
+        int answer = 0, sum = 0;
+
+        for (int i = 0; i < k; i++) {
+            sum+=arr[i];
+        }
+        answer = sum;
+        for (int i = k; i < n; i++) {
+            sum+=(arr[i]-arr[i-k]);
+            answer = Math.max(answer, sum);
+        }
+
+        return answer;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int m = sc.nextInt();
+        int k = sc.nextInt();
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
 
-        System.out.println(solution(n, m, arr));
+        System.out.println(solution2(n, k, arr));
 
     }
 }
