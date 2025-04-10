72bd2923e78ae45958e5b18c8c105eea1a543a07
cheoljundev <cheoljundev@gmail.com>
2025-04-11 00:36:16 +0900

    연속 부분수열 : 내 방법 풀이

---
+++ src/sec3/SumCounter.java
@@ -0,0 +1,39 @@
+package sec3;
+
+import java.util.Scanner;
+
+public class SumCounter {
+    private static int solution(int n, int m, int[] arr) {
+        int answer = 0;
+
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += arr[j];
+
+                if (sum >= m) {
+                    if (sum == m) {
+                        answer++;
+                    }
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println(solution(n, m, arr));
+
+    }
+}
